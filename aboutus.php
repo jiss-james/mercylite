b207b81d841ee0a485d20e97bde01843bf46560d
Jiss James <jissjames360@gmail.com>
2018-02-17 13:15:06 +0000

    Updated about us page

--- aboutus.php
+++ aboutus.php
@@ -34,48 +34,34 @@
               What we are all about!
             </div>
             
-            <br/>
-            <br/>
-            
-            <div id="accordion">
-              <!--First Card-->
-              <div class="card">
-                <div class="card-header text-center" id="headingOne">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                      <i class="fas fa-users"></i> The Team
-                    </button>
-                  </h5>
-                </div>
-                
-                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                  <div class="card-body">
-                    We are a group of TY students from Mount Mercy College, Cork interested in space and technology.<br/>
-                    Our names are : Michelle Cummins, Kate O' Gorman, Iffah Shah, Eimear O' Sullivan, Laura O' Sullivan and Neena Thomas
-                  </div>
-                </div>
+          <div class="alert alert-light" role="alert" data-spy="scroll">
+            <!--Tab and Content-->
+            <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link active" id="pills-team-tab" data-toggle="pill" href="#pills-team" role="tab" aria-controls="pills-team" aria-selected="true">
+                  <i class="fas fa-users"></i>Team
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" id="pills-cansat-tab" data-toggle="pill" href="#pills-cansat" role="tab" aria-controls="pills-cansat" aria-selected="false">
+                  <i class="fas fa-space-shuttle fa-spin"></i>CanSat
+                </a>
+              </li>
+            </ul>
+            <div class="tab-content" id="pills-tabContent">
+              <div class="tab-pane fade show active" id="pills-team" role="tabpanel" aria-labelledby="pills-team-tab">
+                We are a group of TY students from Mount Mercy College, Cork interested in space and technology.<br/>
+                Our names are : Michelle Cummins, Kate O' Gorman, Iffah Shah, Eimear O' Sullivan, Laura O' Sullivan and Neena Thomas
               </div>
-              
-              <!--Second Card-->
-              <div class="card">
-                <div class="card-header text-center" id="headingTwo">
-                  <h5 class="mb-0">
-                    <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      <i class="fas fa-space-shuttle fa-spin"></i> CanSat
-                    </button>
-                  </h5>
-                </div>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-                  <div class="card-body">
-                    A CanSat is a simulation of a real satellite,  contained within the volume of a 330ml soft-drinks can.<br/>
-                    Our Primary Mission is to measure temperature and pressure and transmit the data to a laptop.<br/>
-                    Inside the CanSat, there's an Arduino UNO microcontroller, radio communications module, temperature and pressure sensors and a power supply.
-                    The CanSat will also need a parachute so that it will land safely after launch from high altitide eg. from a rocket or a  drone
-                  </div>
-                </div>
+              <div class="tab-pane fade" id="pills-cansat" role="tabpanel" aria-labelledby="pills-cansat-tab">
+                 A CanSat is a simulation of a real satellite,  contained within the volume of a 330ml soft-drinks can.<br/>
+                Our Primary Mission is to measure temperature and pressure and transmit the data to a laptop.<br/>
+                Inside the CanSat, there's an Arduino UNO microcontroller, radio communications module, temperature and pressure sensors and a power supply.
+                The CanSat will also need a parachute so that it will land safely after launch from high altitide eg. from a rocket or a  drone.
               </div>
-              
-          </div>
+            </div>
+          </div>  
+          
               
         </div>
         
